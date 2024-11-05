8b92331ad91ad31cc507593d74f7c0ef9337137d
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-11-05 18:42:02 +0100

    all done

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,6 +1,14 @@
 const posts = require('../db/storeposts.js');
 const fs = require('fs');
 
+const index = (req,res)=>{
+    const responseData = {
+        data : posts,
+        counter : posts.length
+    };
+    return res.status(200).json(responseData);
+};
+
 const store = (req,res)=>{
     console.log(req.body);
     const post = {
@@ -20,6 +28,34 @@ const store = (req,res)=>{
     });
 };
 
+const update = (req,res)=>{
+    //console.log('go update');
+    const postIndex = posts.findIndex((intem,index)=> intem.id === Number(req.params.id));
+      //se non trova return -1
+    console.log(postIndex);
+    if(postIndex===-1){  //!post sarebbe ambiguo con posts[0] e un post mancante (-1)
+        return res.status(404).json({
+            error : '404 Not Found'
+        });
+    }
+    const newpost = {
+        ...posts[postIndex], // create copy of actual posts[postIndex]
+        title: req.body.title || posts[postIndex].title,  //overwrite only if found posts[postIndex].title
+        slug: req.body.slug || posts[postIndex].slug,
+        content: req.body.content || posts[postIndex].content,
+        image: req.body.image || posts[postIndex].image,
+        tags: req.body.tags || posts[postIndex].tags
+    }
+    posts[postIndex] = newpost;
+    fs.writeFileSync('../db/storeposts.js',`module.exports=${JSON.stringify(posts,null,4)}`);
+    return res.status(200).json({
+        data: newpost
+    });
+};
+
+
 module.exports = {
-    store
+    index,
+    store,
+    update
 }
--- routers/posts.js
+++ routers/posts.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router =  express.Router();
 
 const PostController = require('../controllers/PostController.js');
+
+router.get('/',PostController.index);
 router.post('/', PostController.store);
+router.put('/:id',PostController.update);
 
 module.exports = router;
